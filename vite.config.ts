0a8201a314bb5d381356a059dd5f6f97c03ed029
George M <george.marrows@gmail.com>
2026-05-31 16:07:02 +0200

    Add Solid Devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import devtools from 'solid-devtools/vite';
 
 const clientPort = Number(process.env.CLIENT_PORT ?? 5173);
 const serverOrigin =
   process.env.SERVER_ORIGIN ?? `http://${process.env.HOST ?? "127.0.0.1"}:${process.env.PORT ?? "3000"}`;
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+     devtools({
+      autoname: true, // e.g. enable autoname
+    }),
+    solidPlugin()
+  ],
   root: "client",
   server: {
     port: clientPort,
